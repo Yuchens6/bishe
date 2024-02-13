e2f6cb4174c96d2a382a20f0c598f9271f140043
Yuchens6 <114404755+Yuchens6@users.noreply.github.com>
2024-02-13 10:05:41 +0800

    Update utils_lru.h

--- socialNetwork/src/utils_lru.h
+++ socialNetwork/src/utils_lru.h
@@ -1,1 +1,33 @@
+#ifndef SOCIAL_NETWORK_MICROSERVICES_REDISCLIENT_H
+#define SOCIAL_NETWORK_MICROSERVICES_REDISCLIENT_H
 
+#include "RedisClient.h"
+#include <cpp_redis/cpp_redis>
+
+namespace social_network {
+  
+
+}
+int n = 2;
+        std::vector<std::pair<double, std::string>> sortedKeysWithScores;
+        client.zrevrangebyscore("sorted-keys", "+inf", "-inf", [&](cpp_redis::reply& reply){
+            if (!reply.is_array()) return false;
+            
+            for(auto& element : reply.as_array().elements()) {
+                if(!element.is_array()) continue;
+                
+                double score = element[0].as_double();
+                std::string key = element[1].str();
+                sortedKeysWithScores.push_back({score, key});
+            }
+
+        });
+        
+        // 输出最近最少使用的前n条记录
+        for (size_t i=0; i<n && i<sortedKeysWithScores.size(); ++i) {
+            const auto& pair = sortedKeysWithScores[i];
+            
+        }
+        
+
+    
