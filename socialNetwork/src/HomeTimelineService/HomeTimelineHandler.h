b3e867a69bd1539ece64a4e5b6de4b13991c2762
Yuchens6 <114404755+Yuchens6@users.noreply.github.com>
2024-04-24 20:04:12 +0800

    Update HomeTimelineHandler.h

--- socialNetwork/src/HomeTimelineService/HomeTimelineHandler.h
+++ socialNetwork/src/HomeTimelineService/HomeTimelineHandler.h
@@ -329,7 +329,7 @@ void HomeTimelineHandler::ReadHomeTimeline(
          size_t return_value_length;
          uint32_t flags;
          auto get_span = opentracing::Tracer::Global()->StartSpan(
-             "home-post_storage_mmc_mget_client", {opentracing::ChildOf(&span->context())});
+             "post_storage_mmc_mget_client", {opentracing::ChildOf(&span->context())});
        
          while (true) {
            return_value =
@@ -393,8 +393,11 @@ void HomeTimelineHandler::ReadHomeTimeline(
     for (auto &post_id : post_ids) {
          _return.emplace_back(return_map[post_id]);
        }
-   
-    post_client->ReadPosts(_return, req_id, post_ids_not_cached, writer_text_map);
+   std::vector<int64_t> other_post_ids;
+     for(auto &post_id : post_ids_not_cached){
+         other_post_ids.emplace_back(post_id);
+     }
+    post_client->ReadPosts(_return, req_id, other_post_ids, writer_text_map);
   } catch (...) {
     _post_client_pool->Remove(post_client_wrapper);
     LOG(error) << "Failed to read posts from post-storage-service";
